71fe3bcf3478b419da9c9aa112c21495c83df51e
Everton Cabral <evertoncabral.dev1@gmail.com>
2023-12-04 01:00:03 -0300

    Envio de E-mail
    Ajustes referente ao envio de e-mail

--- ApiPillTime/src/main/java/br/unipar/api/ApiPillTime/email/EmailService.java
+++ ApiPillTime/src/main/java/br/unipar/api/ApiPillTime/email/EmailService.java
@@ -1,7 +1,26 @@
 package br.unipar.api.ApiPillTime.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
 
 
+    @Autowired
+    private MailSender mailSender;
 
-}
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("e.cabral@edu.unipar.br");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+
+
+    }
+
+    }
